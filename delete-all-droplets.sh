4582a29140637b6821916c25b32f6b948d5ca8ec
Amin Gholami <amingh86@yahoo.com>
2020-10-25 19:44:11 +0330

    Create delete-all-droplets.sh

---
+++ delete-all-droplets.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+FIRST_DROPLET_ID={$1}
+LAST_DROPLET_ID={$2}
+YOUR_AUTORIZATION={$3}
+
+for i in $(seq ${FIRST_DROPLET_ID} {$LAST_DROPLET_ID})
+do
+
+#sleep 1
+curl -X DELETE -H "Content-Type: application/json" --connect-timeout 15 --max-time 15 --retry 5 -H "Authorization: Bearer {$YOUR_AUTORIZATION}" "https://api.digitalocean.com/v2/droplets/${i}"
+
+done 
