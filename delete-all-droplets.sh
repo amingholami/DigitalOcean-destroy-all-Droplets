26aa93b3861d53310a2c40a5c033621df4d1d52b
Amin Gholami <amingh86@yahoo.com>
2020-11-19 16:12:42 +0330

    Update delete-all-droplets.sh
    Fix "Unable to authenticate you" error

--- delete-all-droplets.sh
+++ delete-all-droplets.sh
@@ -2,12 +2,12 @@
 
 FIRST_DROPLET_ID=$1
 LAST_DROPLET_ID=$2
-YOUR_AUTORIZATION={$3}
+YOUR_AUTORIZATION=$3
 
 for i in $(seq $FIRST_DROPLET_ID $LAST_DROPLET_ID)
 do
 
 #sleep 1
-curl -X DELETE -H "Content-Type: application/json" --connect-timeout 15 --max-time 15 --retry 5 -H "Authorization: Bearer {$YOUR_AUTORIZATION}" "https://api.digitalocean.com/v2/droplets/${i}"
+curl -X DELETE -H "Content-Type: application/json" --connect-timeout 15 --max-time 15 --retry 5 -H "Authorization: Bearer $YOUR_AUTORIZATION" "https://api.digitalocean.com/v2/droplets/${i}"
 
 done 
